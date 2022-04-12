a7617bc81cc8d13aa00dd6f36a5be3238ae52c32
Sever Miranbeg <sevmirazure@gmail.com>
2022-04-12 19:34:07 +0100

    created blueprints for login and home page

--- app.py
+++ app.py
@@ -1,29 +1,48 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+import uuid
+from flask import Flask, render_template, session, request, redirect, url_for
+from flask_session import Session  # https://pythonhosted.org/Flask-Session
+from auth.azauth import azauth
+from views.home import home
+
+import app_config
+
+
 app = Flask(__name__)
 
+# This section is needed for url_for("foo", _external=True) to automatically
+# generate http scheme when this sample is running on localhost,
+# and to generate https scheme when it is deployed behind reversed proxy.
+# See also https://flask.palletsprojects.com/en/1.0.x/deploying/wsgi-standalone/#proxy-setups
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+def create_app():
+    app = Flask(__name__)
+    #app.config.from_object("config.DevConfig")
+    
+    # register a blueprint for authorization.
+    app.register_blueprint(azauth, url_prefix="/auth")
+    app.register_blueprint(home, url_prefix="")
+    
+    return app
 
 if __name__ == '__main__':
-   app.run()
+    # This is used when running locally only. When deploying to Azure App
+    # Service, a webserver process such as Gunicorn will serve the app.
+    app = create_app()
+    app.config.from_object(app_config)
+    Session(app)
+    app.run(host='localhost', port=5000, debug=True)
+else:
+    app = create_app()
+# [END gae_python3_app]
+# [END gae_python38_app]
+
+
+app.jinja_env.globals.update(_build_auth_code_flow=azauth._build_auth_code_flow)  # Used in template
+
+#if __name__ == "__main__":
+#    app.run()
+
---
+++ views/home.py
@@ -0,0 +1,17 @@
+import traceback
+import requests
+import msal
+
+from flask.blueprints import Blueprint
+from flask import Flask, render_template, request, redirect, session
+from auth.azauth import azauth
+
+home = Blueprint("home", __name__, static_folder="../static/", template_folder="../templates/")
+
+# The default (aka home) route for the app
+@home.route('/', methods=['POST', 'GET'])
+def index():
+    if not session.get("user"):
+        print("returning page auth/login")
+        return redirect("http://localhost:5000/auth/login")
+    return render_template('index.html', user=session["user"], version=msal.__version__)
