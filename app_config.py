64f63e4b9ecb7ba279059d9968c9db8fa19fc542
Sever Miranbeg <sevmirazure@gmail.com>
2022-04-13 23:16:39 +0100

    refactored authorization code and added comments to clarify what the methods are doing

--- app_config.py
+++ app_config.py
@@ -13,9 +13,9 @@ CLIENT_SECRET = "aT17Q~5F-DkJ.uhRJpeNFcbQfGNL-1x658-Nv" # Placeholder - for use 
 AUTHORITY = "https://login.microsoftonline.com/4c57b01d-709a-46da-bb58-e79a9a2f3335"  # For multi-tenant app
 # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirect URI.
-                              # The absolute URL must match the redirect URI you set
-                              # in the app's registration in the Azure portal.
+REDIRECT_PATH = "/getmsaltoken" # Used for forming an absolute URL to your redirect URI.
+                                # The absolute URL must match the redirect URI you set
+                                # in the app's registration in the Azure portal.
 
 # You can find more Microsoft Graph API endpoints from Graph Explorer
 # https://developer.microsoft.com/en-us/graph/graph-explorer
--- auth/azauth.py
+++ auth/azauth.py
@@ -14,19 +14,26 @@ def login():
     session["flow"] = _build_auth_code_flow(scopes=app_config.SCOPE)
     return render_template("login.html", auth_url=session["flow"]["auth_uri"], version=msal.__version__)
 
-@azauth.route(app_config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
+
+# Once a user has been prompted for credentials and authenticate successfully against Azure AD
+# Azure App Registration will contact the the application on this route to pass the credential token
+# that has just been created
+@azauth.route(app_config.REDIRECT_PATH)  # setting the route value using a config file allows us to change the route in one config location only
 def authorized():
     try:
-        cache = _load_cache()
-        result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
+        # the application uses a single cache for the whole instance, so we want to load it
+        cache = loadSessionCache()
+        # get an instance of the confidential client application and call the acquire token by auth code flow method to retieve an
+        # authorisation code. 
+        result = buildAzureMSALApp(cache=cache).acquire_token_by_auth_code_flow(
             session.get("flow", {}), request.args)
         if "error" in result:
-            return render_template("auth_error.html", result=result)
-        session["user"] = result.get("id_token_claims")
-        _save_cache(cache)
-    except ValueError:  # Usually caused by CSRF
-        pass  # Simply ignore them
-    return redirect(url_for("home.index"))
+            return render_template("auth_error.html", result=result) # cleanly redirect to an error page if we hit a problem with the authentication
+        session["user"] = result.get("id_token_claims") # store the user token details in the session cache so it can be picked up later if needed
+        saveAppCache(cache) # update the msal cache 
+    except ValueError:  # not sure why but sometimes an error happens
+        pass  # we can ignore this error and let the code run
+    return redirect(url_for("home.index")) # once user has been authenticated, redirect the browser to the home page
 
 @azauth.route("/logout")
 def logout():
@@ -37,7 +44,7 @@ def logout():
 
 @azauth.route("/graphcall")
 def graphcall():
-    token = _get_token_from_cache(app_config.SCOPE)
+    token = getTokenFromCache(app_config.SCOPE)
     if not token:
         return redirect(url_for("login"))
     graph_data = requests.get(  # Use token to call downstream service
@@ -46,31 +53,37 @@ def graphcall():
         ).json()
     return render_template('display.html', result=graph_data)
 
-def _load_cache():
-    cache = msal.SerializableTokenCache()
+def loadSessionCache():
+    sessionCache = msal.SerializableTokenCache()
     if session.get("token_cache"):
-        cache.deserialize(session["token_cache"])
-    return cache
+        sessionCache.deserialize(session["token_cache"])
+    return sessionCache
 
-def _save_cache(cache):
+# this method checks for a flag that shows the token cachce has changed
+# if it has, then update the session cache with the updated token cache
+def saveAppCache(cache):
     if cache.has_state_changed:
         session["token_cache"] = cache.serialize()
 
-def _build_msal_app(cache=None, authority=None):
+# This method creates an instance of a Confidential client application that represents
+# the Azure App Registration application and will be used to manage credentials and tokens
+def buildAzureMSALApp(cache=None, authority=None):
     return msal.ConfidentialClientApplication(
-        app_config.CLIENT_ID, authority=authority or app_config.AUTHORITY,
-        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
+        app_config.CLIENT_ID, authority=authority or app_config.AUTHORITY, client_credential=app_config.CLIENT_SECRET, token_cache=cache)
 
 def _build_auth_code_flow(authority=None, scopes=None):
-    return _build_msal_app(authority=authority).initiate_auth_code_flow(
+    return buildAzureMSALApp(authority=authority).initiate_auth_code_flow(
         scopes or [],
         redirect_uri=url_for("azauth.authorized", _external=True))
 
-def _get_token_from_cache(scope=None):
-    cache = _load_cache()  # This web app maintains one cache per session
-    cca = _build_msal_app(cache=cache)
-    accounts = cca.get_accounts()
-    if accounts:  # So all account(s) belong to the current signed-in user
-        result = cca.acquire_token_silent(scope, account=accounts[0])
-        _save_cache(cache)
+# if user is already logged in and we have stored a token in the session
+# then we can use this method to find the token in the session to save us
+# time in going back to azure and getting another token
+def getTokenFromCache(scope=None):
+    cache = loadSessionCache()  # This web app maintains one cache per session
+    confidentialClientApplication = buildAzureMSALApp(cache=cache) # get the logical representation of the Azure App Registration that does our authentication and credential management
+    accounts = confidentialClientApplication.get_accounts() # get a list of accounts that have previously signed in and have been stored in cache (https://docs.microsoft.com/en-us/python/api/msal/msal.application.clientapplication?view=azure-python#msal-application-clientapplication-get-accounts)
+    if accounts:
+        result = confidentialClientApplication.acquire_token_silent(scope, account=accounts[0]) # get any existing tokens for the user account that are in the msal cache
+        saveAppCache(cache) # save any updates for the user tokens to the cache
         return result
