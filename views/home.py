e873f0dd24aa28be23377e4be87f38b1924d244d
Sever Miranbeg <sevmirazure@gmail.com>
2022-04-14 17:41:11 +0100

    still fixing app service - home.py has redirect based on url_for

--- views/home.py
+++ views/home.py
@@ -3,7 +3,7 @@ import requests
 import msal
 
 from flask.blueprints import Blueprint
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for
 from auth.azauth import azauth
 
 home = Blueprint("home", __name__, static_folder="../static/", template_folder="../templates/")
@@ -13,5 +13,6 @@ home = Blueprint("home", __name__, static_folder="../static/", template_folder="
 def index():
     if not session.get("user"):
         print("returning page auth/login")
-        return redirect("http://localhost:5000/auth/login")
+        #return redirect("http://localhost:5000/auth/login")
+        return redirect(url_for("azauth.login"))
     return render_template('index.html', user=session["user"], version=msal.__version__)
