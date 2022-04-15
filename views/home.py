392a4427d802be60e48b88bafefb1370bbb60bb8
Sever Miranbeg <sevmirazure@gmail.com>
2022-04-15 15:28:58 +0100

    still fixing app service - changed session package code to call correct flask-caching package. but this might not work when published to azure and it imports its own libraries

--- views/home.py
+++ views/home.py
@@ -11,9 +11,8 @@ home = Blueprint("home", __name__, static_folder="../static/", template_folder="
 # The default (aka home) route for the app
 @home.route('/', methods=['POST', 'GET'])
 def index():
-    print (str(session["user"]))
-    if not session.get("user") is None:
-        print("returning page auth/login ---- HERE IS STUFF " + str(session.get("user")))
+    if not session.get("user"):
+        print("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH returning page auth/login ---- HERE IS STUFF " + str(session.get("user")))
         #return redirect("http://localhost:5000/auth/login")
         return redirect(url_for("azauth.login"))
     return render_template('index.html', user=session["user"], version=msal.__version__)
