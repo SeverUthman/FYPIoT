2b224b7f5931e288403566e50c1e7ef0dd5c74ae
Sever Miranbeg <sevmirazure@gmail.com>
2022-04-15 17:09:20 +0100

    fixing web app - added config from file

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import app_config
 
 app = Flask(__name__)
 app.secret_key = 'aT17Q~5F-DkJ.uhRJpeNFcbQfGNL-1x658-Nv' 
+app.config.from_object(app_config)
+
 # This section is needed for url_for("foo", _external=True) to automatically
 # generate http scheme when this sample is running on localhost,
 # and to generate https scheme when it is deployed behind reversed proxy.
@@ -21,6 +23,7 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 def create_app():
     app = Flask(__name__)   
     app.secret_key = 'aT17Q~5F-DkJ.uhRJpeNFcbQfGNL-1x658-Nv' 
+    app.config.from_object(app_config)
     # register a blueprint for authorization.
     app.register_blueprint(azauth, url_prefix="/auth")
     # register the home/index blueprint
@@ -34,6 +37,7 @@ if __name__ == '__main__':
     app = create_app()
     app.config.from_object(app_config)
     app.secret_key = 'aT17Q~5F-DkJ.uhRJpeNFcbQfGNL-1x658-Nv' 
+    app.config.from_object(app_config)
     Session(app)
     app.run(host='localhost', port=5000, debug=True)
 else:
