5fe2bf47a34bebe6af7454a12f1d0e726558d2dc
Sever Miranbeg <sevmirazure@gmail.com>
2022-04-21 16:21:22 +0100

    reworked sqlalchemy to correctly use association object ORM model, can now get values into and out of intermediate tables

--- database/db.py
+++ database/db.py
@@ -1,7 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import app, Flask
 from datetime import datetime
+from sqlalchemy.orm import relationship
 #from flask_sqlalchemy_session import flask_scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 """
 from app import create_app
@@ -19,26 +22,39 @@ db = SQLAlchemy()
 def IsUserRegistered():
     return False
 
-user_kitchen = db.Table('user_kitchen',
+""" user_kitchen = db.Table('user_kitchen',
             db.Column('user_kitchen_id', db.Integer, primary_key=True),
             db.Column('is_default_kitchen', db.Boolean),
             db.Column('kitchen_id', db.Integer, db.ForeignKey('kitchen.kitchen_id')),
             db.Column('user_id', db.Integer, db.ForeignKey('user.user_id'))
-)
+) """
+
+class user_kitchen(db.Model):
+    __tablename__ = "user_kitchen"
+    kitchen_id = db.Column(db.ForeignKey('kitchen.kitchen_id'), primary_key=True)
+    user_id = db.Column(db.ForeignKey('user.user_id'), primary_key=True)
+    is_default_kitchen = db.Column('is_default_kitchen', db.Boolean)
+
+    kitchen = relationship("kitchen", backref="user_associations")
+    user = relationship("user", backref="kitchen_associations")
+
 
 class user(db.Model):
+    __tablename__ = "user"
     user_id = db.Column(db.Integer, primary_key=True)
     user_az_id = db.Column(db.String(100))
     is_admin = db.Column(db.Boolean)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     email = db.Column(db.String(50))
-    kitchens = db.relationship("kitchen",secondary=user_kitchen)
+
+    kitchens = relationship("kitchen", secondary="user_kitchen", lazy='dynamic')
 
     def __repr__(self):
         return '<user %r>' % self.user_id
 
 class kitchen(db.Model):
+    __tablename__ = "kitchen"
     kitchen_id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(100))
     line1 = db.Column(db.String(50))
@@ -46,6 +62,8 @@ class kitchen(db.Model):
     city = db.Column(db.String(50))
     postcode = db.Column(db.String(50))
     country = db.Column(db.String(50))
+
+    users = relationship("user", secondary="user_kitchen")
     
     def __repr__(self):
         return '<kitchen %r>' % self.kitchen_id
--- kitchen/kitchenmanagement.py
+++ kitchen/kitchenmanagement.py
@@ -16,12 +16,30 @@ def registerkitchen():
             city = request.form['city']
             postcode = request.form['postcode']
             country = request.form['country']
-            defaultkitchen = request.form['defaultKitchen']
-            new_kitchen = db.kitchen(nickname=name, line1=firstline, line2=secondline, city=city, postcode=postcode, country=country)
+            defaultkitchen = request.form.get('defaultKitchen')
+            new_kitchen = db.kitchen(nickname=name, line1=firstline, line2=secondline, city=city, postcode=postcode, country=country)      
             db.db.session.add(new_kitchen)
 
             currentuser = db.user.query.filter_by(user_az_id=session["user_id"]).first()
             currentuser.kitchens.append(new_kitchen)
+            #thing = db.user_kitchen(filter(lambda x: x.kitchen_id == new_kitchen.kitchen_id, currentuser.kitchen_associations)) #.query.filter_by(kitchen_id=new_kitchen.kitchen_id).first()
+
+            if defaultkitchen:
+                notdefaultkitchen = db.db.session.query(db.user_kitchen).filter( 
+                    db.user_kitchen.user_id == currentuser.user_id,
+                    db.user_kitchen.is_default_kitchen == True).first()
+                notdefaultkitchen.is_default_kitchen = False
+
+                userkitchens = db.db.session.query(db.user_kitchen).filter(
+                    db.user_kitchen.kitchen_id == new_kitchen.kitchen_id, 
+                    db.user_kitchen.user_id == currentuser.user_id).first()
+                userkitchens.is_default_kitchen = True
+            else:
+                userkitchens = db.db.session.query(db.user_kitchen).filter(
+                    db.user_kitchen.kitchen_id == new_kitchen.kitchen_id, 
+                    db.user_kitchen.user_id == currentuser.user_id).first()
+                userkitchens.is_default_kitchen = False
+
             db.db.session.commit()
             """
             if defaultkitchen:
@@ -48,13 +66,27 @@ def showkitchen(kitchid):
         kitchenovens = db.kitchen_appliance.query.filter_by(kitchen_id=kitchid, kitchen_appliance_type_id=1).all()
         kitchenfridges = db.kitchen_appliance.query.filter_by(kitchen_id=kitchid, kitchen_appliance_type_id=2).all()
         kitchenscales = db.kitchen_appliance.query.filter_by(kitchen_id=kitchid, kitchen_appliance_type_id=3).all()
-        #user = db.user.query.filter_by(user_az_id=session['user_id'])
+        user = db.user.query.filter_by(user_az_id=session['user_id']).first()
+        print(user)
+        kid=kitchen.kitchen_id
+        uid=user.user_id
+
+        query =  db.db.session.query(db.user_kitchen).filter(
+            db.user_kitchen.kitchen_id == kid, 
+            db.user_kitchen.user_id == uid)
+
+        print(query)
+        
+        userkitchens = db.db.session.query(db.user_kitchen).filter(
+            db.user_kitchen.kitchen_id == kid, 
+            db.user_kitchen.user_id == uid).first()
         #query = 'SELECT * from user_kitchen where kitchen_id = {} AND user_id = {}'.format(kitchid, user.user_id)
         #stuff = db.db.session.execute(query)
         #kitchenuser = db.kitchen.query.join(db.user_kitchen).join(db.kitchen).filter((db.user_kitchen.c.kitchen_id==kitchid)).all()
         if not kitchen:
             return redirect('/')
-        return render_template("showkitchen.html", kitchen=kitchen, ovens=kitchenovens, fridges=kitchenfridges, scales=kitchenscales)#, kitchenuser=kitchenuser)
+        isdefaultkitchen = userkitchens.is_default_kitchen
+        return render_template("showkitchen.html", kitchen=kitchen, isdefaultkitchen=isdefaultkitchen, ovens=kitchenovens, fridges=kitchenfridges, scales=kitchenscales)#, kitchenuser=kitchenuser)
     except Exception as e:
         return render_template("errorpage.html", errorstack=e)
 
