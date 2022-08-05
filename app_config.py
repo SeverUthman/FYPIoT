e848b6f8bed0d4edb7a16d2bf64a76399aa64abe
SeverUthman <85617282+SeverUthman@users.noreply.github.com>
2022-08-05 11:14:30 +0100

    Removing sensitive information from public repo

--- app_config.py
+++ app_config.py
@@ -2,7 +2,7 @@ import os
 
 CLIENT_ID = "a7bdf031-d1b4-49fa-b06c-a2f82a2cd1ac" # Application (client) ID of the App Registration in Azure Active Directory
 
-CLIENT_SECRET = "aT17Q~5F-DkJ.uhRJpeNFcbQfGNL-1x658-Nv" # This client secret was generated when creating the App Registration in Azure Active Directory. It cannot be found again via Azure Portal and would need to generate a new one if it's lost.
+CLIENT_SECRET = 'removing from public repo' # This client secret was generated when creating the App Registration in Azure Active Directory. It cannot be found again via Azure Portal and would need to generate a new one if it's lost.
 
 AUTHORITY = "https://login.microsoftonline.com/4c57b01d-709a-46da-bb58-e79a9a2f3335"  # The Tenant ID of the App Registration in Azure Active Directory
 
@@ -24,14 +24,14 @@ APPLICATIONENDPOINT = 'https://graph.microsoft.com/beta/applications' # To be us
 CREATEUSERENDPOINT = ''
 SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
 
-SQLALCHEMY_DATABASE_URI = 'mssql+pymssql://kitchencontroladmin:Manager99@kitchencontrolsql.database.windows.net/fyp-kitchencontrol-db?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mssql+pymssql://username:password@azuresqlinstance.database.windows.net/databasename?charset=utf8'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 KITCHENCONTROLADMINGROUPNAME = "KitchenControlAdmin"
 
-IOTHUBHOSTNAME = 'KitchenControlHub.azure-devices.net'
-IOTHUBCONN = 'HostName=KitchenControlHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=3YTaq+xP4DrH2LBb/CjZTdPzryB4a8qCGXuCNYKnfkY='
+IOTHUBHOSTNAME = 'iothubname.azure-devices.net'
+IOTHUBCONN = 'HostName=KitchenControlHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=GENERATETHISFROMAZURE'
 
 OVEN_APPLIANCE_TYPE_ID = 1
 FRIDGE_APPLIANCE_TYPE_ID = 2
